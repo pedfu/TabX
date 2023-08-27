4745255a233c112ca8a6483e28ddf9927e0ab206
Pedro Fuziwara <fuziwarapedro@gmail.com>
2023-08-27 10:56:02 -0300

    fix: Replace icons for svg

--- newtab.js
+++ newtab.js
@@ -66,13 +66,23 @@ function expandButtonClickHandler(event) {
     const content = component.querySelector('.carousel-content')
     if (component) {
         const expandListButton = component.querySelector('.button-expand')
-        if (expandListButton?.classList.contains('fa-angle-down')) {
-            expandListButton?.classList.remove('fa-angle-down')
-            expandListButton?.classList.add('fa-angle-up')
+        if (expandListButton?.classList.contains('down')) {
+            expandListButton?.classList.remove('down')
+            expandListButton?.classList.add('up')
+            expandListButton.innerHTML = `
+            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 15L12 9L18 15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            `
             content.style.height = '13rem'
         } else {
-            expandListButton?.classList.remove('fa-angle-up')
-            expandListButton?.classList.add('fa-angle-down')
+            expandListButton?.classList.remove('up')
+            expandListButton?.classList.add('down')
+            expandListButton.innerHTML = `
+            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 9L12 15L18 9" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            `
             content.style.height = '0rem'
         }
         const id = Number(component.id.replace('list-', ''));
@@ -145,21 +155,33 @@ function loadLists() {
         header.classList.add('carousel-header')
         header.innerHTML = `
             <div class="title-container">
-                <i class="button-expand fa-solid fa-angle-up"></i>
+                <button class="button-expand up">
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 15L12 9L18 15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
                 <p class="list-title">${list.name}</p>
             </div>
             <div class="carousel-controls">
                 <button class="del">
-                    <i class="fa-regular fa-trash-can"></i>
+                    <svg width="22px" height="22px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                 </button>
                 <button class="add">
-                    <i class="fa-solid fa-plus"></i>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 12H20M12 4V20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                 </button>
                 <button class="prev disabled">
-                    <i class="fa-solid fa-angles-left"></i>
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 17L13 12L18 7M11 17L6 12L11 7" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                 </button>
                 <button class="next">
-                    <i class="fa-solid fa-angles-right"></i>
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 17L11 12L6 7M13 17L18 12L13 7" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                 </button>
             </div>`
 
--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    console.log('teste')
     const newTabUrl = "chrome://newtab/";
     const newTab = await chrome.tabs.create({ url: newTabUrl });
 })
